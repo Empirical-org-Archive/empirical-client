73a2fade6664109ec8f4a2c0bb2a8294e46b77b2
James Cox <james@imaj.es>
2014-08-09 15:32:58 -0400

    initial gem stub from bundler

---
+++ lib/quill/client.rb
@@ -0,0 +1,7 @@
+require "quill/client/version"
+
+module Quill
+  module Client
+    # Your code goes here...
+  end
+end
---
+++ lib/quill/client/version.rb
@@ -0,0 +1,5 @@
+module Quill
+  module Client
+    VERSION = "0.0.1"
+  end
+end
